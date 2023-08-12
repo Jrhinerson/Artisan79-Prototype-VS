5d4d77ba1d0894e00d91e53c90e4ef270d9bd980
Jrhinerson <jordan.rhinerson@gmail.com>
2023-08-12 12:57:15 -0400

    Establish Fetch to .Net API

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,33 @@
+'use client'
+
+import { useEffect, useState } from "react"
+
 
 export default function Home() {
+  const [products, setProducts] = useState([
+    {name: 'product1', price: 100.00},
+    {name: 'product2', price: 200.00},
+  ]);
+
+  useEffect(() => {
+    fetch('http://localhost:5160/api/Products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, [])
+
+  function addProduct() {
+    setProducts(prevState => [...prevState, {name: 'product' + (prevState.length + 1), price: (prevState.length * 100 + 100)}])
+  }
+  
   return (
-    <main>
-      
-    </main>
+    <>
+      <h1>Artisan79</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>{item.name} - {item.price}</li>
+        ))}
+        <button onClick={addProduct}>Add Product</button>
+      </ul>
+    </>
   )
 }
